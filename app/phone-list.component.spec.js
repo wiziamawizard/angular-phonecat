19088021c561ae404f71172d64b8e37704ae4270
Wiz <seongjihyeon913@gmail.com>
2022-07-17 23:57:18 +0900

    3 - Components

---
+++ app/phone-list.component.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('phoneList', function () {
+  beforeEach(module('phonecatApp'));
+
+  describe('PhoneListController', function () {
+    it('should create a `phones` model with 3 phones', inject(function (
+      $componentController
+    ) {
+      var ctrl = $componentController('phoneList');
+
+      expect(ctrl.phones.length).toBe(3);
+    }));
+  });
+});

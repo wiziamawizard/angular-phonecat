00abc830fe1f57eb18d9e4125cc24d1bd37efc60
wiz <wiziamawizard@gmail.com>
2022-07-19 10:06:58 +0900

    5 - Filtering Repeaters

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,12 +1,23 @@
 'use strict';
 
-// AngularJS E2E Testing Guide:
-// https://docs.angularjs.org/guide/e2e-testing
+describe('PhoneCat Application', function () {
+  describe('phoneList', function () {
+    beforeEach(function () {
+      browser.get('index.html');
+    });
 
-describe('My app', function() {
+    it('should filter the phone list as a user types into the search box', function () {
+      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+      var query = element(by.model('$ctrl.query'));
 
-  beforeEach(function() {
-    browser.get('index.html');
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
   });
-
 });
